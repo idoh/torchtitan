fee30702ebef995cf0f8e1efa3af8db1aa3b3d73
Jiani Wang <jianiw@meta.com>
2025-06-24 10:16:02 -0700

    try CP

--- torchtitan/models/deepseek_v3/infra/parallelize.py
+++ torchtitan/models/deepseek_v3/infra/parallelize.py
@@ -24,12 +24,12 @@ def parallelize_deepseekv3(
 
     dp_mesh: DeviceMesh | None = None
     if (
-        parallel_dims.dp_shard_enabled
+        parallel_dims.dp_shard_enabled or parallel_dims.cp_enabled
     ):  # apply FSDP or HSDP, potentially with Context Parallel
         if parallel_dims.dp_replicate_enabled:
-            dp_mesh_dim_names = ("dp_replicate", "dp_shard")
+            dp_mesh_dim_names = ("dp_replicate", "dp_shard_cp")
         else:
-            dp_mesh_dim_names = ("dp_shard",)
+            dp_mesh_dim_names = ("dp_shard_cp",)
         dp_mesh = world_mesh[tuple(dp_mesh_dim_names)]
 
         apply_fsdp(
